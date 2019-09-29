185f2b1eb2bb3ec9ef3edfc278e94c830e773ea0
AginS <agins.main@gmail.com>
2019-09-29 16:00:36 +0300

    Добавил динамическое выделение памяти

--- main.cpp
+++ main.cpp
@@ -4,20 +4,24 @@
 
 using namespace std;
 
-void updateArraySize(string* p_array, int* size );
+string *updateArraySize(string* p_array, int *size );
 void print(string *word, string *antonym);
 
 int main() {
-    setlocale(LC_ALL, "rus"); // Может правда дело в этом?
+    setlocale(LC_ALL, "rus"); // Строка в память о тех, кто пользуется виндой (F to pay respect)
 
     std::string line;
 
     std::ifstream in("dict.txt");
 
-    string sizeSTR;
-    getline(in, sizeSTR); // Получение первой строки
-    sizeSTR = sizeSTR.substr(1); //убираем знак &
-    const int size = stoi(sizeSTR);
+    //string sizeSTR;
+    //getline(in, sizeSTR); // Получение первой строки
+    //sizeSTR = sizeSTR.substr(1); //убираем знак &
+
+    //int size = stoi(sizeSTR);
+
+    int size = 2;
+
     //string array[ size ];
     string *p_array = new string[size];
     int i = 0;
@@ -26,6 +30,11 @@ int main() {
     {
         while (getline(in, line))
         {
+            if (size == i + 1 )
+            {
+                p_array = updateArraySize(p_array, & size);
+            }
+            cout << "Присваивание line" << endl;
             p_array[i] = line;
             i++;
         }
@@ -38,9 +47,10 @@ int main() {
 
     for (int i = 0; i < size; i++)
     {
+        cout << "I: " << i << endl;
         cout << "Array: " << p_array[i] << endl;
         cout << "Len: " << p_array[i].length() << endl;
-        cout << "Find word: " << p_array[i].find(word) << endl;
+        cout << "Find word: " << p_array[i].find(word) << endl << endl << endl;
 
         if ( p_array[i].find(word) < p_array[i].length() ) // find почему-то если строка не соддержит word возвращает безумное занчение
         {
@@ -66,4 +76,20 @@ int main() {
 void print(string *word, string *antonym)
 {
     cout << "Антоним к \"" << *word << "\" это слово \"" << *antonym << "\"" <<endl;
+}
+
+string *updateArraySize(string* p_array, int *size )
+{
+    cout << "Обновление массива запущенно" << endl;
+    *size *= 2;
+    string *p_new_array = new string[ *size ];
+    cout << "Создан массив с размером: " << *size << endl;
+    for (int i = 0; i < (*size/2); ++i)
+    {
+       // cout << "Копированвние " << p_array[i] << endl;
+        p_new_array[i] = p_array[i];
+    }
+    delete[] p_array;
+    cout << "Обновление заверенно"<< endl;
+    return p_new_array;
 }
